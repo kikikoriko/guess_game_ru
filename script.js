b246c17bcf6b0016916e026990c9f7a0c42b8628
kikikoriko <95526039+kikikoriko@users.noreply.github.com>
2022-03-31 19:00:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correcto!😒';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 10;
+document.querySelector('.score').textContent = 30;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (!guess) {
+    displayMessage('Нет числа! 🦡');
+  } else if (guess === secretNumber) {
+    displayMessage('Верно!😒');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.guess').value = '';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(
+        guess > secretNumber ? 'Слишком много! ☝️' : 'Слишком мало! ☝️'
+      );
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.guess').value = '';
+    } else {
+      displayMessage('Облом! 😘');
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.guess').value = '';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  displayMessage('Введи число...');
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+});
